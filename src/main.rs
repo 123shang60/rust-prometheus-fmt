e5c82737f4238834edd508b1d9b4246a7af21177
123shang60 <616761475@qq.com>
2020-12-04 20:53:43 +0800

    feat : actix web

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use actix_web::{web, App, HttpServer, Responder};
 use prometheus_exporter_base::prelude::*;
 use psutil::memory::VirtualMemory;
 use psutil::*;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().route("/metrics", web::get().to(metrics)))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+        .unwrap();
+    Ok(())
+}
+
+async fn metrics() -> impl Responder {
     let swap_memory: VirtualMemory = memory::virtual_memory().unwrap();
-    println!("{:?}", swap_memory);
 
     let mut pc = PrometheusMetric::build()
         .with_name("memory_use")
@@ -15,15 +25,13 @@ fn main() {
     pc.render_and_append_instance(
         &PrometheusInstance::new()
             .with_label("type", "total")
-            .with_value(swap_memory.total() / 1024 / 1024 / 1024),
+            .with_value(swap_memory.total()),
     );
 
     pc.render_and_append_instance(
         &PrometheusInstance::new()
             .with_label("type", "free")
-            .with_value(swap_memory.free() / 1024 / 1024 / 1024),
+            .with_value(swap_memory.free()),
     );
-
-    println!("{:?}", pc.render());
-    println!("Hello, world!");
+    pc.render()
 }
